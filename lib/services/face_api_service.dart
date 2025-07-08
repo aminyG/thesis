928b667bfdfc9b6e8cfe017f0379cd08b38f9c6e
Aminy Ghaisan <69082746+aminyG@users.noreply.github.com>
2025-07-08 15:32:52 +0700

    commit

--- lib/services/face_api_service.dart
+++ lib/services/face_api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class FaceApiService {
   final Dio _dio = Dio(BaseOptions(
@@ -39,32 +41,57 @@ class FaceApiService {
   }
 
   // Fungsi untuk verifikasi wajah
-  Future<Map<String, dynamic>> verifyFace(
-      File imageFile, String identifier) async {
-    final formData = FormData.fromMap({
-      'photo':
-          await MultipartFile.fromFile(imageFile.path, filename: 'face.jpg'),
-      'identifier': identifier,
-    });
+  // Future<Map<String, dynamic>> verifyFace(
+  //     File imageFile, String identifier) async {
+  //   final formData = FormData.fromMap({
+  //     'photo':
+  //         await MultipartFile.fromFile(imageFile.path, filename: 'face.jpg'),
+  //     'identifier': identifier,
+  //   });
 
+  //   try {
+  //     final response = await _dio.post('/users/verify-face', data: formData);
+
+  //     // Menyimpan respons sebagai Map
+  //     if (response.statusCode == 200) {
+  //       return {
+  //         'status_code': response.statusCode,
+  //         'message': response.data['message'],
+  //         'data': response.data['data'],
+  //       };
+  //     } else {
+  //       return {
+  //         'status_code': response.statusCode,
+  //         'message': 'Verifikasi gagal'
+  //       };
+  //     }
+  //   } catch (e) {
+  //     throw Exception('Verification failed: $e');
+  //   }
+  // }
+
+  Future<bool> verifyFace(String faceImage) async {
     try {
-      final response = await _dio.post('/users/verify-face', data: formData);
-
-      // Menyimpan respons sebagai Map
+      final response = await http.post(
+        Uri.parse('https://presence.guestallow.com/api/face/verifyFace'),
+        body: jsonEncode({
+          'face_image': faceImage,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
       if (response.statusCode == 200) {
-        return {
-          'status_code': response.statusCode,
-          'message': response.data['message'],
-          'data': response.data['data'],
-        };
+        return true; // Successful response
       } else {
-        return {
-          'status_code': response.statusCode,
-          'message': 'Verifikasi gagal'
-        };
+        // Log or show a meaningful error
+        print('Error verifying face: ${response.body}');
+        return false;
       }
     } catch (e) {
-      throw Exception('Verification failed: $e');
+      // Catch any errors like network issues
+      print('Error during face verification: $e');
+      return false;
     }
   }
 }
